a0eba560daf716e07a93a80d44e5f8530f83ae5f
Ramona Bellamy <bellamy2727@yahoo.com>
2015-05-20 19:23:59 -0500

    finished hw

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,6 +9,7 @@ function update ()
 	var time = new Date();
 	var seconds = time.getSeconds();
 	var hours = time.getHours();
+	var minutes = time.getMinutes();
 
 	hue+= 3;
 	if(hue > 360){
@@ -17,12 +18,25 @@ function update ()
 	//changes hue
 	document.body.style.backgroundColor= 'hsl('+hue+', 100%, 50%)';
 	console.log(document.body.style.backgroundColor);
-	//adds 0 to seconds below 10
+
+
+	//adds 0 to seconds/mins/hours below 10
 	if(seconds < 10){
 		seconds = '0' + seconds;
 	}
-	clock.innerHTML = time.getHours()+':' + time.getMinutes()+ ':' + seconds.toString();
+	if( minutes < 10){
+
+		minutes = '0' + minutes;
+	}
+	if(hours < 10){
+
+		hours = '0' + hours;
+	}
+	clock.innerHTML = hours+':' + minutes+ ':' + seconds;
 
 }
 
-setInterval(update, 1000)
+setInterval(update, 1000);
+
+
+
